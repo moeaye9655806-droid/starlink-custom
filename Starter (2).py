51edfcc76369d0c5f9136d3445cca3befd324aee
moeaye9655806-droid <moeaye9655806@gmail.com>
2026-06-17 18:56:23 +0630

    Update Starter (2).py

--- Starter (2).py
+++ Starter (2).py
@@ -8,13 +8,27 @@ import requests
 import webbrowser
 from urllib.parse import urlparse, parse_qs
 
-# ပြီးမှ Class စတင်ပါ
 class LOD_V32_FIXED:
     def __init__(self):
-        # ... ကျန်တဲ့ ကုဒ်များ ...
+        self.is_running = False
+        self.target_url = "http://192.168.1.1"
+        self.tg_owner = "@guts9984"
+        self.tg_channel = "https://t.me/+guts9984"
+        
+    def brute_force_engine(self):
+        self.is_running = True
+        while self.is_running:
+            part1 = "".join(random.choices("0123456789", k=6))
+            part2 = "".join(random.choices("0123456789", k=9))
+            part3 = "".join(random.choices("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", k=6))
+            code = part1 + part2 + part3
+            print(f"Testing: {code}")
+            time.sleep(0.1)
 
+if __name__ == "__main__":
+    app = LOD_V32_FIXED()
+    app.brute_force_engine()
 
-_RAW_SOURCE = """
 
 
 
